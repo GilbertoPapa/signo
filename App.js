e1a66607f0365e5e3c5e70711ddaef63fa21b2f5
Gilberto Papa <gilberto.papa@digiage.com.br>
2019-05-11 12:07:11 -0300

    Criando Rotas

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Switch } from 'react-native-gesture-handler';
+import { StyleSheet, View } from 'react-native';
+import ListaSignos from './ListaSignos'
+import {NativeRouter as Router,Switch, Route} from  'react-router-native'
 
 export default class App extends React.Component {
   render() {
